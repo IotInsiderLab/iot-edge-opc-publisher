3e56958dbfb26ae2d5604aa5445011d95c1e92a0
v-krgali <v-krgali@DESKTOP-J47I49O>
2018-07-19 09:26:00 +0200

    added arburg credentials

--- opcpublisher/OpcSession.cs
+++ opcpublisher/OpcSession.cs
@@ -595,8 +595,8 @@ namespace OpcPublisher
                             false,
                             PublisherOpcApplicationConfiguration.ApplicationName,
                             timeout,
-                            new UserIdentity(new AnonymousIdentityToken()),
-                           // new UserIdentity("host_computer", "123"),
+                         //   new UserIdentity(new AnonymousIdentityToken()),
+                            new UserIdentity("host_computer", "123"),
                             null);
                 }
                 catch (Exception e)
